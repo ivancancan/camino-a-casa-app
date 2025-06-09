3a2954954e340b3b12c8fa5e76a34b8549988cf1
Ivan Gerardo Cantu Cantu <ivangerardocantucantu@Ivans-MacBook-Air.local>
2025-06-08 20:38:35 -0600

    commit buenazo

--- app.config.js
+++ app.config.js
@@ -4,21 +4,49 @@ dotenvFlow.config({
   node_env: process.env.NODE_ENV || 'staging',
 });
 
-console.log('Variables de entorno (NODE_ENV):', process.env.NODE_ENV);
-console.log('API_URL:', process.env.API_URL);
-console.log('SUPABASE_URL:', process.env.SUPABASE_URL);
-console.log('SUPABASE_ANON_KEY:', process.env.SUPABASE_ANON_KEY);
+const env = process.env.NODE_ENV || 'staging';
 
-export default ({ config }) => {
-  const env = process.env.NODE_ENV || 'staging';
-
-  return {
-    ...config,
-    extra: {
-      API_URL: process.env.API_URL || process.env.EXPO_PUBLIC_API,
-      SUPABASE_URL: process.env.SUPABASE_URL,
-      SUPABASE_ANON_KEY: process.env.SUPABASE_ANON_KEY,
-      ENV: env,
+export default ({ config }) => ({
+  ...config,
+  name: "Arya",
+  slug: "arya",
+  owner: "ivancancan",
+  scheme: "com.arya.app",
+  version: "1.0.0",
+  orientation: "portrait",
+  icon: "./assets/arya.png",
+  userInterfaceStyle: "light",
+  newArchEnabled: true,
+  splash: {
+    image: "./assets/arya-splash-2.png",
+    resizeMode: "contain",
+    backgroundColor: "#f8f2ff",
+  },
+  ios: {
+    supportsTablet: true,
+    bundleIdentifier: "com.ivancancan.aryaapp",
+    infoPlist: {
+      ITSAppUsesNonExemptEncryption: false,
     },
-  };
-};
+  },
+  android: {
+    package: "com.arya.app",
+    adaptiveIcon: {
+      foregroundImage: "./assets/arya-adaptive-icon.png",
+      backgroundColor: "#ffffff",
+    },
+    edgeToEdgeEnabled: true,
+  },
+  web: {
+    favicon: "./assets/arya.png",
+  },
+  extra: {
+    eas: {
+      projectId: "4d84b3fe-1670-4aa7-9c52-f44a8071869c",
+    },
+    API_URL: process.env.API_URL || process.env.EXPO_PUBLIC_API,
+    SUPABASE_URL: process.env.SUPABASE_URL,
+    SUPABASE_ANON_KEY: process.env.SUPABASE_ANON_KEY,
+    ENV: env,
+  },
+});
